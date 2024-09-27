c92d46a182e1fce3d236af62ab34355348df5de7
Sasmann38 <169153341+Heglinger@users.noreply.github.com>
2024-09-27 10:19:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
+ const socket = io();
 
-const socket = io();
+        const chat = document.getElementById('chat');
+        const messageForm = document.getElementById('message-form');
+        const messageInput = document.getElementById('message-input');
 
+        // Display message from server
+        socket.on('message', (message) => {
+            const p = document.createElement('p');
+            p.textContent = message;
+            chat.appendChild(p);
+            chat.scrollTop = chat.scrollHeight; // Auto scroll to the bottom
+        });
 
-const chat = document.getElementById('chat');
-const messageForm = document.getElementById('message-form');
-const messageInput = document.getElementById('message-input');
-
-
-let username = document.getElementById('usernameselect').value; 
-
-// Display message from server
-    socket.on('message', function(message) {
-    var p = document.createElement('p');
-    p.textContent = message;
-    chat.appendChild(p);
-    chat.scrollTop = chat.scrollHeight; // Auto scroll to the bottom
-});
-
-// Send message to server
-    messageForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    let message = messageInput.value.trim();
-    
-    // Fügen Sie den Benutzernamen hinzu, wenn er nicht vorhanden ist
-    if (!message.includes(username)) {
-        message = username + ": " + message;
-    }
-    
-    socket.emit('chatMessage', message);
-    messageInput.value = '';
-    messageInput.focus();
-});
-
+        // Send message to server
+        messageForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const msg = messageInput.value;
+            socket.emit('chatMessage', msg);
+            messageInput.value = '';
+            messageInput.focus();
+        });
