b3752d9cbb1d866edea3cf56c15d78ca36ed3ca8
Sasmann38 <169153341+Heglinger@users.noreply.github.com>
2024-11-06 14:27:56 +0100

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,22 +1,22 @@
-const socket = io();
+ const socket = io();
 
-    const chat = document.getElementById('chat');
-    const messageForm = document.getElementById('message-form');
-    const messageInput = document.getElementById('message-input');
+        const chat = document.getElementById('chat');
+        const messageForm = document.getElementById('message-form');
+        const messageInput = document.getElementById('message-input');
 
-    // Display message from server
-    socket.on('message', (message) => {
-        const p = document.createElement('p');
-        p.textContent = message;
-        chat.appendChild(p);
-        chat.scrollTop = chat.scrollHeight; // Auto scroll to the bottom
-    });
+        // Display message from server
+        socket.on('message', (message) => {
+            const p = document.createElement('p');
+            p.textContent = message;
+            chat.appendChild(p);
+            chat.scrollTop = chat.scrollHeight; // Auto scroll to the bottom
+        });
 
-    // Send message to server
-    messageForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const msg = messageInput.value;
-        socket.emit('chatMessage', msg);
-        messageInput.value = '';
-        messageInput.focus();
-    });
+        // Send message to server
+        messageForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const msg = messageInput.value;
+            socket.emit('chatMessage', msg);
+            messageInput.value = '';
+            messageInput.focus();
+        });
