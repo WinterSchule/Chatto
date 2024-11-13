539d86522f7306ebb8f42cc9c9c446fcceed0959
WinterSchule <168537815+WinterSchule@users.noreply.github.com>
2024-11-13 13:36:24 +0100

    Delete public directory

--- public/index.js
+++
@@ -1,8 +0,0 @@
-
-// Hamburger Menü Funktionalität
-const burgerMenu = document.getElementById('burger-menu');
-const navLinks = document.querySelector('.nav-links');
-
-burgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Toggle der "active" Klasse für das Menü
-});
